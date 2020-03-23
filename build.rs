fbeabcd213ab9153b103174c56bfc6dc9f61a3e2
Adam Gausmann <agausmann@fastmail.com>
2020-03-23 12:02:03 -0500

    Update for new emscripten / rust versions / bindgen

--- build.rs
+++ build.rs
@@ -3,23 +3,38 @@ extern crate bindgen;
 use std::env;
 use std::path::Path;
 
+use bindgen::RustTarget;
+
 fn main() {
     let out_dir = env::var("OUT_DIR").expect("failed to get envvar OUT_DIR");
-    let emscripten_dir = env::var("EMSCRIPTEN").expect("failed to get envvar EMSCRIPTEN");
+    let emsdk_dir = env::var("EMSDK").expect("failed to get envvar EMSDK");
 
     let whitelist = "^_?em_|^_?emscripten_|^_?EM_|^_?EMSCRIPTEN_";
 
     let builder = bindgen::builder()
         .header("etc/emscripten.h")
         .generate_comments(true)
-        .whitelisted_type(whitelist)
-        .whitelisted_function(whitelist)
-        .whitelisted_var(whitelist)
-        .no_unstable_rust()
+        .whitelist_type(whitelist)
+        .whitelist_function(whitelist)
+        .whitelist_var(whitelist)
+        .rust_target(RustTarget::Stable_1_40)
         .use_core()
-        .clang_arg(format!("-I{}/system/include", emscripten_dir))
-        .clang_arg(format!("-I{}/system/include/libc", emscripten_dir))
-        .clang_arg(format!("-I{}/system/include/libcxx", emscripten_dir))
+        .clang_arg(format!(
+            "-I{}/upstream/emscripten/system/include",
+            emsdk_dir
+        ))
+        .clang_arg(format!(
+            "-I{}/upstream/emscripten/system/include/libc",
+            emsdk_dir
+        ))
+        .clang_arg(format!(
+            "-I{}/upstream/emscripten/system/lib/libc/musl/arch/emscripten",
+            emsdk_dir
+        ))
+        .clang_arg(format!(
+            "-I{}/upstream/emscripten/system/include/libcxx",
+            emsdk_dir
+        ))
         .clang_arg("-x")
         .clang_arg("c++")
         .clang_arg("-std=c++11")
