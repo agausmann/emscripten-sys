f7c4a73f1fc0791c67387d2d6e12ee17b80a07f7
Adam Gausmann <agausmann@fastmail.com>
2020-03-24 22:18:12 -0500

    Fix the whitelist regex passed to bindgen
    Anchors are added implicitly, meaning the regex must match the _full_ identifier.

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").expect("failed to get envvar OUT_DIR");
     let emscripten_dir = env::var("EMSCRIPTEN").expect("failed to get envvar EMSCRIPTEN");
 
-    let whitelist = "^_?em_|^_?emscripten_|^_?EM_|^_?EMSCRIPTEN_";
+    let whitelist = "_?em_.*|_?emscripten_.*|_?EM_.*|_?EMSCRIPTEN_.*";
 
     let builder = bindgen::builder()
         .header("etc/emscripten.h")
